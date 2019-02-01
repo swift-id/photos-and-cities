6d97414c0096051c4db64e489cf2f44ce0d45239
R. Kukuh <rkukuh@gmail.com>
2019-02-01 15:09:59 +0700

    Initial Commit

---
+++ Photos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Photos
+//
+//  Created by R. Kukuh on 01/02/19.
+//  Copyright © 2019 R. Kukuh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
